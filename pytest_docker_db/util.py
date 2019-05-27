1dffda4fb464c8a2f90ba22f7902ae8f23443b5d
Kyle Prestel <kprestel@gmail.com>
2019-05-27 09:18:14 -0400

    fix flake8

--- pytest_docker_db/util.py
+++ pytest_docker_db/util.py
@@ -76,7 +76,7 @@ def is_pathname_valid(pathname: str) -> bool:
                     return False
     # If a "TypeError" exception was raised, it almost certainly has the
     # error message "embedded NUL character" indicating an invalid pathname.
-    except TypeError as exc:
+    except TypeError:
         return False
     # If no exception was raised, all path components and hence this
     # pathname itself are valid.
